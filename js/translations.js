72d771be2e906cd76fdc35a93260ce1f31cea2b7
LatinoF <39635923+LatinoF@users.noreply.github.com>
2025-02-10 14:39:39 +0100

    Updated

--- js/translations.js
+++ js/translations.js
@@ -2,11 +2,9 @@ var res = {
     en: {
         translation: {
             "menu.options": "Options",
+            "menu.language": "Language:",
             "menu.autocomplete": "Autocomplete",
-            "menu.save-to-file": "Save to file",
-            "menu.mode.label": "File type",
-            "menu.mode.aiken": "AIKEN",
-            "menu.mode.gift": "GIFT",
+            "menu.save-to-file": "Save & Download",
             
             "error.first-line-empty": "First line can't be empty",
             "error.question-must-be-in-one-line": "Question must be in one line",
@@ -36,11 +34,9 @@ var res = {
     it: {
         translation: {
             "menu.options": "Opzioni",
-            "menu.autocomplete": "Autocompleta",
-            "menu.save-to-file": "Salva e scarica",
-            "menu.mode.label": "Tipo",
-            "menu.mode.aiken": "AIKEN",
-            "menu.mode.gift": "GIFT",
+            "menu.language": "Lingua:",
+            "menu.autocomplete": "Autocompletamento",
+            "menu.save-to-file": "Salva & Scarica",
 
             "error.first-line-empty": "La prima riga non può essere vuota",
             "error.question-must-be-in-one-line": "La domanda deve essere su una sola riga",
